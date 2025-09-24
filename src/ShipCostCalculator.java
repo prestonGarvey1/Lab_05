36d706806782a0f38c1a6fdb6cd1f7b32ef9c8c8
prestonGarvy1 <thebozzkid2010@gmail.com>
2025-09-23 22:59:59 -0400

    finish ShipCostCalculator.java

--- src/ShipCostCalculator.java
+++ src/ShipCostCalculator.java
@@ -9,9 +9,10 @@ public class ShipCostCalculator {
 
         //Create values
         double itemPrice = 0;
-        double taxRate;
+        double taxRate = 0.02;
         double finalTax;
         double finalPrice;
+
 
         if (scan.hasNextDouble()) {
             itemPrice = scan.nextDouble();
@@ -22,13 +23,20 @@ public class ShipCostCalculator {
             } else {
                 System.out.println("You have entered an incorrect number");
             }
-        }
-        else{
+        } else {
             System.out.println("You have entered an incorrect value");
         }
-        if(itemPrice > 100){
-        System.out.println("Your Number is higher than 100, free shipping is not available.");
 
+
+        if (itemPrice > 100) {
+            finalTax = itemPrice * taxRate;
+            finalPrice = itemPrice + finalTax;
+            System.out.println("Your Number is higher than 100, free shipping is not available.");
+            System.out.println("Your purchase is $" + finalPrice + " with the added shipping fee.");
+        } else {
+            finalPrice = itemPrice;
+            System.out.println("Your purchase has free shipping!");
+            System.out.println("Your purchase is $" + finalPrice + " with free shipping.");
         }
 
     }
